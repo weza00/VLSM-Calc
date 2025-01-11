c9e35f480eedcae50d4da78205630b3c70b4eab7
weza00 <mateo.recalde.op@gmail.com>
2025-01-11 17:53:11 -0500

    Completado el form Datos con sus funciones respectivas (sujeto a cambios futuros).

--- VLSM Calc/Form1.Designer.cs
+++ VLSM Calc/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
